6a3a6b9a3be40ff710853c3ec0526d374431d672
ze-pena <jose.pena.dev@gmail.com>
2025-12-26 19:33:13 -0300

    feat: create Avatar global component

---
+++ src/global/components/Avatar/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+
+import { IAuthor } from "@/interfaces/Post";
+
+interface Props {
+  author: IAuthor;
+}
+
+const avatarFallback = "/assets/images/icon_fallback.png";
+
+export default function Avatar({ author }: Props) {
+  return (
+    <div className={styles.avatar}>
+      <Image
+        className={styles.avatarImage}
+        src={author.avatar ?? avatarFallback}
+        alt="Avatar do usuário"
+        width={36}
+        height={36}
+      />
+
+      <span className={styles.avatarUsername}>@{author.username}</span>
+    </div>
+  );
+}
