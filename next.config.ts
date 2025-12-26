4129e3db4653c9d0e08344321c46126bd2a8ad27
ze-pena <jose.pena.dev@gmail.com>
2025-12-26 19:29:27 -0300

    feat: apply local and remote path pattern

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    localPatterns: [{ pathname: "/assets/**", search: "" }],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/viniciosneves/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
