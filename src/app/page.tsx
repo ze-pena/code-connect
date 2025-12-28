99bdbc03dedfe44bba0445124d490eb3a7c4578b
ze-pena <jose.pena.dev@gmail.com>
2025-12-28 11:40:24 -0300

    feat: build new layout and create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,76 @@
-import Image from "next/image";
+import Link from "next/link";
+import logger from "@/logger";
+
+import Card from "@/global/components/Card";
+
+import { IPost } from "@/interfaces/Post";
+import { IPaginatedResponse } from "@/interfaces/Response";
+
 import styles from "./page.module.css";
 
-export default function Home() {
+type FetchPostResponse = Promise<IPaginatedResponse<IPost>>;
+
+const baseUrl = "http://localhost:3042";
+
+async function fetchPosts(page: number): FetchPostResponse {
+  try {
+    const response = await fetch(`${baseUrl}/posts?_page=${page}&_per_page=6`);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch post data!");
+    }
+
+    logger.info("This request was a success!");
+    return response.json() as FetchPostResponse;
+  } catch (error: unknown) {
+    let errorMessage = "An unexpected error has occurred";
+
+    if (error instanceof Error) errorMessage = error.message;
+
+    logger.error(errorMessage);
+    return {} as FetchPostResponse;
+  }
+}
+
+interface PageParams {
+  searchParams: { page?: string };
+}
+
+export default async function Home({ searchParams }: PageParams) {
+  const { page } = await searchParams;
+  const {
+    data: postList,
+    prev,
+    next,
+  } = await fetchPosts(page ? Number(page) : 1);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      <ul className={styles.list}>
+        {Array.isArray(postList) &&
+          postList.map((post) => (
+            <li key={post.id} className={styles.listItem}>
+              <Card post={post} />
+            </li>
+          ))}
+      </ul>
+
+      <nav className={styles.pagination}>
+        <li
+          className={`${styles.button} ${
+            prev ? styles.active : styles.inactive
+          }`}
+        >
+          <Link href={`/?page=${prev}`}>Página anterior</Link>
+        </li>
+        <li
+          className={`${styles.button} ${
+            next ? styles.active : styles.inactive
+          }`}
+        >
+          <Link href={`/?page=${next}`}>Próxima página</Link>
+        </li>
+      </nav>
     </div>
   );
 }
