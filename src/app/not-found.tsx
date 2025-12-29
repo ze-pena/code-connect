0fe36bac5acc9f316a4b4bd51f96c494f6274c47
ze-pena <jose.pena.dev@gmail.com>
2025-12-29 19:33:01 -0300

    feat: create a default not found laytout to the home page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import LinkButton from "@/global/components/LinkButton";
+
+import styles from "./page.module.css";
+
+export default function NotFound() {
+  return (
+    <div className={`${styles.page} ${styles.problem}`}>
+      <Image
+        src="/assets/images/not_found.png"
+        width={657}
+        height={367}
+        alt="Página não encontrada"
+        loading="eager"
+      />
+      <h1>OPS! Página não encontrada.</h1>
+      <p>Você pode voltar ao feed e continuar buscando projetos incríveis!</p>
+      <LinkButton path={"/"} label="Voltar ao feed" />
+    </div>
+  );
+}
