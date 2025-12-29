4bf6094684d9f206afafa1e556de965e18092824
ze-pena <jose.pena.dev@gmail.com>
2025-12-28 22:56:50 -0300

    chore: installing and configuring prisma

---
+++ prisma/db.ts
@@ -0,0 +1,8 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../src/generated/prisma/client";
+
+const connectionString = process.env["DATABASE_URL"] ?? "";
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
