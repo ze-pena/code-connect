25c649de08c349bc9a74f0f3165e532cfe85614e
ze-pena <jose.pena.dev@gmail.com>
2026-01-04 02:10:30 -0300

    chore: update prisma config to equals with the vercel configurations

--- prisma.config.ts
+++ prisma.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
     seed: "tsx prisma/seed.ts",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["POSTGRES_PRISMA_URL"],
   },
 });
